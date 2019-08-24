3898050886c94ec75a5f7d58a205eaeab17d6c3b
kdc <2540053270@qq.com>
2019-08-24 16:15:54 +0800

    Test 1

---
+++ src/com/kdc/SendEmail2.java
@@ -0,0 +1,69 @@
+package com.kdc;
+// 需要用户名密码邮件发送实例
+//文件名 SendEmail2.java
+//本实例以QQ邮箱为例，你需要在qq后台设置
+ 
+import java.util.Properties;
+ 
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+ 
+public class SendEmail2
+{
+   public static void main(String [] args)
+   {
+      // 收件人电子邮箱
+      String to = "songer93@foxmail.com";
+ 
+      // 发件人电子邮箱
+      String from = "2540053270@qq.com";
+ 
+      // 指定发送邮件的主机为 smtp.qq.com
+      String host = "smtp.qq.com";  //QQ 邮件服务器
+ 
+      // 获取系统属性
+      Properties properties = System.getProperties();
+ 
+      // 设置邮件服务器
+      properties.setProperty("mail.smtp.host", host);
+ 
+      properties.put("mail.smtp.auth", "true");
+      // 获取默认session对象
+      Session session = Session.getDefaultInstance(properties,new Authenticator(){
+        public PasswordAuthentication getPasswordAuthentication()
+        {
+         return new PasswordAuthentication("2540053270@qq.com", "pvyeyihossyaecci"); //发件人邮件用户名、授权码
+        }
+       });
+ 
+      try{
+         // 创建默认的 MimeMessage 对象
+         MimeMessage message = new MimeMessage(session);
+ 
+         // Set From: 头部头字段
+         message.setFrom(new InternetAddress(from));
+ 
+         // Set To: 头部头字段
+         message.addRecipient(Message.RecipientType.TO,
+                                  new InternetAddress(to));
+ 
+         // Set Subject: 头部头字段
+         message.setSubject("宝哥，不好意思啊，装一比！！！！");
+ 
+         // 设置消息体
+         message.setText("This is actual message");
+ 
+         // 发送消息
+         Transport.send(message);
+         System.out.println("Sent message successfully....from runoob.com");
+      }catch (MessagingException mex) {
+         mex.printStackTrace();
+      }
+   }
+}
